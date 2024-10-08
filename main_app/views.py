f2cdfb448a4f7058e426d0fea92acb6629e9f713
Ayodele Owolabi <joyowolabi@gmail.com>
2024-10-08 11:11:40 -0400

    Auth and Nav

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+import requests
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
+
+class Home(LoginView):
+    template_name = 'home.html'
+
+
+def signup(request):
+    error_message=""
+    if request.method =='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'signup.html', context)
